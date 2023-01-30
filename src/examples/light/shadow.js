c736937e7a39f9abc5ced27eade1f5bd0c7141da
wuyifan <wuyifan@max-optics.com>
2023-01-30 16:51:44 +0800

    feat: add OBJLoader demo

---
+++ src/examples/light/shadow.js
@@ -0,0 +1,138 @@
+/*
+ * @Date: 2023-01-30 14:03:05
+ * @LastEditors: wuyifan wuyifan@max-optics.com
+ * @LastEditTime: 2023-01-30 16:51:08
+ * @FilePath: /threejs-demo/src/examples/light/shadow.js
+ */
+/* eslint-disable no-unused-vars */
+import dat from "../../lib/util/dat.gui.js";
+import { OrbitControls } from "../../lib/three/OrbitControls.js";
+import {
+    initRenderer,
+    initPerspectiveCamera,
+    createAxesHelper,
+    addLargeGroundPlane,
+    resize,
+} from "../../lib/tools/index.js";
+import {
+    Scene,
+    Vector3,
+    Mesh,
+    MeshLambertMaterial,
+    SpotLight,
+    AmbientLight,
+    SpotLightHelper,
+    BoxGeometry,
+    Color,
+} from "../../lib/three/three.module.js";
+
+window.onload = () => {
+    init();
+};
+
+var stop = false;
+
+const init = () => {
+    const renderer = initRenderer();
+    // 1
+    renderer.shadowMap.enabled = true;
+    const camera = initPerspectiveCamera(new Vector3(20, 20, 20));
+    camera.up.set(0, 0, 1);
+
+    const scene = new Scene();
+    createAxesHelper(scene);
+    const ambientLight = new AmbientLight(0x3c3c3c);
+    scene.add(ambientLight);
+
+    const spotLight = new SpotLight(0xffffff, 1, 180, 0.5);
+    spotLight.shadow.mapSize.height = 2048;
+    spotLight.shadow.mapSize.width = 2048;
+    spotLight.position.set(-40, 30, 30);
+    spotLight.castShadow = true;
+    scene.add(spotLight);
+    const spotLightHelper = new SpotLightHelper(spotLight)
+    scene.add(spotLightHelper);
+
+    const groundPlane = addLargeGroundPlane(scene);
+    groundPlane.position.set(0, 0, 0);
+    groundPlane.rotation.z = -0.5 * Math.PI;
+    // 2
+    groundPlane.receiveShadow = true;
+ 
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    draw(scene, spotLight, spotLightHelper);
+    resize(renderer, camera);
+    render();
+    function render() {
+        controls.update();
+        if(!stop){
+            spotLight.position.copy(camera.position);
+            spotLightHelper.update();
+        }
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
+    }
+
+    window.camera = camera;
+    window.scene = scene;
+};
+
+function draw(scene, light, helper) {
+
+
+    const material = new MeshLambertMaterial({ color: "#016050" });
+
+    const mesh = new Mesh(new BoxGeometry(5, 5, 5), material);
+    mesh.position.set(0, 5, 3);
+    mesh.name = "box";
+    mesh.receiveShadow = true;
+    // 3
+    mesh.castShadow = true;
+    mesh.renderOrder = 1;
+    scene.add(mesh);
+
+    const mesh2 = new Mesh(new BoxGeometry(1, 1, 1), material);
+    mesh2.position.set(5, 5, 3);
+    mesh2.name = "box2";
+    // 4
+    mesh2.receiveShadow = true;
+
+    mesh2.castShadow = true;
+    mesh2.renderOrder = 2;
+    scene.add(mesh2);
+
+    const gui = new dat.GUI();
+    
+    light.target = mesh;
+
+    const controls = {
+        color: light.color,
+        stop
+    }
+
+    const updateHelper = e =>  helper.update();
+
+    gui.addColor(controls, 'color').onChange(e => {
+        light.color = new Color(e.r, e.g, e.b);
+    });
+    gui.add(light, 'decay', 0, 15.01);
+    gui.add(light, 'angle', 0, Math.PI * 2).onChange(updateHelper);
+    gui.add(light, 'intensity', 0, 5).onChange(updateHelper);
+    gui.add(light, 'penumbra', 0, 1).onChange(updateHelper);
+    gui.add(light, 'distance', 0, 200).onChange(updateHelper);
+    gui.add(light, 'castShadow');
+
+    gui.add(light, 'target',['mesh','mesh2']).onChange(e=>{
+        light.target = e === 'mesh' ? mesh : mesh2;
+        updateHelper()
+    });
+
+    gui.add(helper, 'visible').name('show helper');
+
+    gui.add(controls,'stop').onChange(e=>{
+        stop = e
+    })
+
+
+}
--- src/examples/loader/OBJLoader.js
+++ src/examples/loader/OBJLoader.js
@@ -1,83 +1,158 @@
 /*
- * @Date: 2023-01-09 14:37:51
+ * @Date: 2023-01-30 14:03:05
  * @LastEditors: wuyifan wuyifan@max-optics.com
- * @LastEditTime: 2023-01-29 18:29:46
- * @FilePath: /threejs-demo/src/examples/loader/OBJLoader.js
+ * @LastEditTime: 2023-01-30 16:43:03
+ * @FilePath: /threejs-demo/src/examples/light/shadow copy.js
  */
+/* eslint-disable no-unused-vars */
+import dat from "../../lib/util/dat.gui.js";
+import { OBJLoader } from "../../lib/three/OBJLoader.js";
+import { OrbitControls } from "../../lib/three/OrbitControls.js";
+import {
+    initRenderer,
+    initPerspectiveCamera,
+    resize,
+} from "../../lib/tools/index.js";
 import {
     Scene,
-    PointLight,
-    PerspectiveCamera,
-    BufferGeometry,
-    PointsMaterial,
-    AdditiveBlending,
-    Points,
-    MeshLambertMaterial,
-    Float32BufferAttribute,
-    AmbientLight,
-    MeshBasicMaterial,
+    Vector3,
     Mesh,
-    DirectionalLight,
+    Shape,
+    SpotLight,
+    AmbientLight,
+    SpotLightHelper,
+    ShapeGeometry,
     MeshPhongMaterial,
-  } from "../../lib/three/three.module.js";
-  import { OrbitControls } from "../../lib/three/OrbitControls.js";
-  import { OBJLoader } from "../../lib/three/OBJLoader.js";
-  import { initRenderer, resize } from "../../lib/tools/index.js";
-  
-  window.onload = () => {
+    Color,
+    Matrix4
+} from "../../lib/three/three.module.js";
+
+window.onload = () => {
     init();
-  };
-  
-  function init() {
+};
+
+var stop = false;
+
+const init = () => {
     const renderer = initRenderer();
-    renderer.autoClear = false;
-    const camera = new PerspectiveCamera(
-      45,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      10000
-    );
-    camera.position.set(0, 0, 120);
-    camera.lookAt(0, 0, 0);
+    // 1
+    renderer.shadowMap.enabled = true;
+    const camera = initPerspectiveCamera(new Vector3(100, 1, 60));
+    camera.up.set(0, 0, 1);
+
     const scene = new Scene();
-    renderer.setClearColor(0x000000);
-    const ambientLight = new AmbientLight(0xffffff, 1);
+    const ambientLight = new AmbientLight(0x3c3c3c);
     scene.add(ambientLight);
-  
+
+    const spotLight = new SpotLight(0xffffff, 1, 180, 0.68,0,0.2);
+    spotLight.shadow.mapSize.height = 2048;
+    spotLight.shadow.mapSize.width = 2048;
+    spotLight.position.set(25, 10, 100);
+    spotLight.castShadow = true;
+    scene.add(spotLight);
+    const spotLightHelper = new SpotLightHelper(spotLight);
+    spotLightHelper.visible = false;
+    scene.add(spotLightHelper);
+
+    const circleRadius = 120;
+    const circleShape = new Shape()
+      .moveTo(0, circleRadius)
+      .absarc(0, 0, circleRadius, 0, Math.PI * 2, false);
+    const groundPlane = new Mesh(
+      new ShapeGeometry(circleShape),
+      new MeshPhongMaterial()
+    );
+
+    scene.add(groundPlane)
+    groundPlane.position.set(0, 0, -28);
+    groundPlane.rotation.z = -0.5 * Math.PI;
+    // 2
+    groundPlane.receiveShadow = true;
+ 
+
     const controls = new OrbitControls(camera, renderer.domElement);
-    draw(scene, camera);
+    draw(scene, spotLight, spotLightHelper);
     resize(renderer, camera);
-  
     render();
     function render() {
-      controls.update();
-      renderer.clear();
-      renderer.render(scene, camera);
-      requestAnimationFrame(render);
+        controls.update();
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
     }
-  
+
     window.camera = camera;
     window.scene = scene;
-  }
-  
-  function draw(scene,camera) {
-  
-    // model
+};
+
+function draw(scene, light, helper) {
     const modelPath = "../../resources/models/Mountain lion.OBJ";
     const loader = new OBJLoader();
-    loader.load(modelPath, (obj)=>{
-        scene.add(obj);
-        console.log(obj);
-    },
-    (ProgressEvent) => {
-        console.log(
-            "progress: " + (ProgressEvent.loaded / ProgressEvent.total) * 100 + " %"
-        );
-    },
-    (url) => {
-        console.log("Opp ! have an Error in" + url);
+    const newMin = new Vector3();
+    const newMax = new Vector3();
+    new Promise((resolve, reject) => {
+      loader.load(
+        modelPath,
+        (obj) => {
+          /// 模型太大了，缩放一下
+          obj.scale.set(0.1, 0.1, 0.1);
+          obj.rotateX(Math.PI / 2);
+          scene.add(obj);
+          const modelMatrix = new Matrix4().compose(obj.position, obj.quaternion, obj.scale);
+  
+        //   obj.children[0].material.color.set(new Color("gray"));
+          // 重新计算模型最新位置，并放在中心点
+          obj.children[0].geometry.computeBoundingBox();
+          const { max, min } = obj.children[0].geometry.boundingBox;
+          // 3
+          obj.children[0].castShadow = true // 投射阴影
+          obj.children[0].receiveShadow = true 
+          newMin.set(max.x, max.y, max.z).applyMatrix4(modelMatrix);
+          newMin.set(min.x, min.y, min.z).applyMatrix4(modelMatrix);
+  
+          const x = (newMax.x - newMin.x) / 2 - 10;
+          const y = (newMax.y - newMin.y) / 2;
+          const z = (newMax.z - newMin.z) / 2;
+          console.log(x,y,z);
+          obj.position.set(x, y, z);
+          resolve(obj);
+        },
+        (ProgressEvent) => {
+          // console.log(
+          //   "progress: " + (ProgressEvent.loaded / ProgressEvent.total) * 100 + " %"
+          // );
+        },
+        (error) => {
+          console.log("Opp ! have an Error in" + error);
+          reject(error);
+        }
+      );
+    })
+
+    const gui = new dat.GUI();
+
+
+    const controls = {
+        color: light.color,
+        stop
     }
-    );
-  }
-  
-  
+
+    const updateHelper = e =>  helper.update();
+
+    gui.addColor(controls, 'color').onChange(e => {
+        light.color = new Color(e.r, e.g, e.b);
+    });
+    gui.add(light, 'decay', 0, 15.01);
+    gui.add(light, 'angle', 0, Math.PI * 2).onChange(updateHelper);
+    gui.add(light, 'intensity', 0, 5).onChange(updateHelper);
+    gui.add(light, 'penumbra', 0, 1).onChange(updateHelper);
+    gui.add(light, 'distance', 0, 200).onChange(updateHelper);
+    gui.add(light, 'castShadow');
+
+    gui.add(helper, 'visible').name('show helper');
+
+    gui.add(light.position, "x", -200, 200, 0.1).onChange(updateHelper);
+    gui.add(light.position, "y", -200, 200, 0.1).onChange(updateHelper);
+    gui.add(light.position, "z", -200, 200, 0.1).onChange(updateHelper);
+
+
+}
