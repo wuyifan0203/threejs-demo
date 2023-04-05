8eb3443c1cd7248e64d3fd10e2cab29d0b551ed8
wuyifan <1208097313@qq.com>
2023-04-06 00:18:36 +0800

    feat:添加三种线的实例使用

---
+++ packages/examples/line/useLine.js
@@ -0,0 +1,88 @@
+/*
+ * @Date: 2022-11-16 15:00:21
+ * @LastEditors: wuyifan 1208097313@qq.com
+ * @LastEditTime: 2023-04-05 11:17:15
+ * @FilePath: /threejs-demo/src/examples/edge/index2.js
+ */
+
+import {
+  Scene,
+  Vector3,
+  LineBasicMaterial,
+  EdgesGeometry,
+  LineSegments,
+  BoxGeometry,
+  BufferGeometry,
+  BufferAttribute,
+  Path,
+  Line,
+  LineLoop,
+} from '../../lib/three/three.module.js';
+import { OrbitControls } from '../../lib/three/OrbitControls.js';
+import {
+  initRenderer,
+  initOrthographicCamera,
+  initCustomGrid,
+  createAxesHelper,
+} from '../../lib/tools/index.js';
+
+
+(function () {
+  init();
+}());
+
+function init() {
+  const renderer = initRenderer();
+  const camera = initOrthographicCamera(new Vector3(100, -100, 100));
+  camera.up.set(0, 0, 1);
+  const scene = new Scene();
+  createAxesHelper(scene);
+  renderer.setClearColor(0xffffff);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  draw(scene);
+
+  render();
+  function render() {
+    controls.update();
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+  }
+}
+
+function draw(scene) {
+  const LineMaterial = new LineBasicMaterial({ color: 'orange' });
+
+  const vertex = [
+    -5, 5, 0, -5, -5, 0,   // 第一条线
+    5, -5, 0, 5, 5, 0      // 第二条线
+  ]
+
+
+  const gemotry = new BufferGeometry().setAttribute('position', new BufferAttribute(new Float32Array(vertex), 3));
+
+  const path = new Path();
+  path.absarc(0, 0, 5, 0, 2 * Math.PI, false);
+  const gemotry2 = new BufferGeometry().setFromPoints(path.getPoints());
+
+  const ls = new LineSegments(gemotry, LineMaterial);
+  const ls2 = new LineSegments(gemotry2,LineMaterial);
+
+  const l = new Line(gemotry,LineMaterial);
+  const l2 = new Line(gemotry2,LineMaterial);
+
+  const lp = new LineLoop(gemotry,LineMaterial)
+  const lp2 = new LineLoop(gemotry2,LineMaterial);
+
+  ls2.position.z = 2;
+  l2.position.z = 2;
+  l.position.x = l2.position.x = 12;
+  lp.position.x = lp2.position.x = -12;
+  lp2.position.z = 2;
+
+  scene.add(ls,ls2);
+  scene.add(l,l2);
+  scene.add(lp,lp2)
+
+
+}
