14ab1ca4b26dff17027ac78b1ec2c3f8dd5057b5
wuyifan <wuyifan@max-optics.com>
2023-05-15 19:02:48 +0800

    feat：tween

---
+++ packages/examples/tween/useTween.js
@@ -0,0 +1,96 @@
+/* eslint-disable camelcase */
+/*
+ * @Date: 2023-05-08 17:17:11
+ * @LastEditors: Yifan Wu 1208097313@qq.com
+ * @LastEditTime: 2023-05-15 18:42:17
+ * @FilePath: /threejs-demo/packages/examples/tween/useTween.js
+ */
+import { OrbitControls } from '../../lib/three/OrbitControls.js';
+import {
+  AmbientLight,
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  MeshLambertMaterial,
+  PointLight,
+  Scene,
+  Vector3,
+} from '../../lib/three/three.module.js';
+import {
+  initRenderer,
+  initOrthographicCamera,
+  resize,
+  initCustomGrid,
+} from '../../lib/tools/index.js';
+import {
+  Tween,
+  update,
+} from '../../lib/other/tween.esm.js';
+
+window.onload = () => {
+  init();
+};
+
+function init() {
+  const renderer = initRenderer({ logarithmicDepthBuffer: true });
+  renderer.setClearColor(0xffffff);
+  const camera = initOrthographicCamera(new Vector3(30, -30, 30));
+  camera.lookAt(0, 0, 0);
+  camera.up.set(0, 0, 1);
+
+  const scene = new Scene();
+  scene.add(new AmbientLight());
+  const light = new PointLight();
+  light.position.set(10, 10, 10);
+  scene.add(light);
+
+  initCustomGrid(scene);
+
+  const orbitControls = new OrbitControls(camera, renderer.domElement);
+
+  const createMaterial = (color) => new MeshLambertMaterial({ color });
+
+  const geometry = new BoxGeometry(1, 1, 1);
+  const mesh_test_to = new Mesh(geometry, createMaterial('green'));
+  const mesh_test_delay = new Mesh(geometry, createMaterial('red'));
+
+  scene.add(mesh_test_to);
+  scene.add(mesh_test_delay);
+
+  useTweenTo(mesh_test_to);
+
+  resize(renderer, camera);
+
+  function render() {
+    orbitControls.update();
+    update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
+
+  render();
+
+  window.scene = scene;
+  window.camera = camera;
+}
+
+function useTweenTo(mesh) {
+  const tween = new Tween({ x: -10 });
+  const tween2 = new Tween({ x: 10 });
+
+  tween.to({ x: 10 }, 2000);
+  tween2.to({ x: -10 }, 2000);
+
+  const tweenUpdate = (obj) => {
+    mesh.position.x = obj.x;
+  };
+
+  tween.onUpdate(tweenUpdate);
+
+  tween2.onUpdate(tweenUpdate);
+
+  tween.chain(tween2);
+  tween2.chain(tween);
+
+  tween.start();
+}
