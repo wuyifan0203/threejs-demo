5f51b8d5821178f5f24d0ce78012583faf5d9401
wuyifan <1208097313@qq.com>
2023-08-22 01:23:55 +0800

    feat(editor):添加tree

--- apps/f-editor/src/App.vue
+++ apps/f-editor/src/App.vue
@@ -1,49 +1,61 @@
 <!--
  * @Date: 2023-06-09 11:26:39
  * @LastEditors: Yifan Wu 1208097313@qq.com
- * @LastEditTime: 2023-08-21 01:40:26
+ * @LastEditTime: 2023-08-22 01:03:44
  * @FilePath: /threejs-demo/apps/f-editor/src/App.vue
 -->
 <template>
-  <n-message-provider>
-    <Layout />
-  </n-message-provider>
+  <NThemeEditor class="editor">
+    <n-message-provider>
+      <Layout />
+    </n-message-provider>
+  </NThemeEditor>
 </template>
 
 <script lang="ts">
 import Layout from "./layout/index.vue";
-import {NMessageProvider} from 'naive-ui'
+import { NMessageProvider } from 'naive-ui'
 import { defineComponent, onMounted } from 'vue';
+import { NThemeEditor } from 'naive-ui'
 export default defineComponent({
   name: 'App',
   components: {
     Layout,
-    NMessageProvider
+    NMessageProvider,
+    NThemeEditor
   },
   setup() {
-    onMounted(()=>{
+    onMounted(() => {
       window.document.documentElement.setAttribute("data-theme", 'drak');
     })
   }
 })
 </script>
 
-<style >
-
+<style lang="scss" >
 @import "./assets/scss/mixin.scss";
 @import "golden-layout/dist/css/goldenlayout-base.css";
 @import "golden-layout/dist/css/themes/goldenlayout-dark-theme.css";
+
 body {
   height: 100%;
   margin: 0;
   overflow: hidden;
 }
 
+.n-config-provider {
+  height: 100vh;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  font-size: 12px!important;
+  font-size: 12px !important;
   height: 100vh;
   width: 100vw;
+  font-size: 18px;
+  @include font_color("font_color1");
+  @include background_color("background_color1");
+  @include border_color("border_color1");
 }
 </style>
--- apps/f-editor/src/layout/objectTree/index.vue
+++ apps/f-editor/src/layout/objectTree/index.vue
@@ -1,20 +1,68 @@
+<!--
+ * @Date: 2023-08-16 21:31:59
+ * @LastEditors: Yifan Wu 1208097313@qq.com
+ * @LastEditTime: 2023-08-22 00:51:16
+ * @FilePath: /threejs-demo/apps/f-editor/src/layout/objectTree/index.vue
+-->
 <template>
-<div>this is Object Tree</div>
+  <div>this is Object Tree</div>
+  <n-tree block-line :data="data" :selectable="false" />
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue';
+import { NTree } from 'naive-ui'
 export default defineComponent({
-    name: 'ObjectTree' ,
-    components: {},
-    props:[],
-    setup() {
-      return {
+  name: 'ObjectTree',
+  components: {
+    NTree
+  },
+  props: [],
+  setup() {
 
+    const data = [
+      {
+        label: 'Scene',
+        key: 'id',
+        visable: true,
+        children: [
+          {
+            label: 'Camera',
+            key: 'q',
+          },
+          {
+            label: 'MEsh',
+            key: 'w',
+          },
+          {
+            label: 'Object',
+            key: 'e',
+          },
+          {
+            label: 'Circle',
+            key: 'r',
+          },
+          {
+            label: 'Camera',
+            key: 't',
+          },
+          {
+            label: 'Camera',
+            key: 'id1',
+          }
+        ]
       }
+    ]
+    return {
+      data
+
     }
+  }
 })
 </script>
 <style scoped>
+.n-tree{
+  --n-arrow-color: #000;
+}
 
 </style>
