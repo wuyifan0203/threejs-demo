45aebbf65f779d2b5f690dc6c8fcf42026d07fae
wuyifan <1208097313@qq.com>
2023-12-25 02:05:30 +0800

    feat(example):初步添加galaxy2

--- src/particle/galaxy.js
+++ src/particle/galaxy.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2023-12-24 01:07:12
  * @LastEditors: Yifan Wu 1208097313@qq.com
- * @LastEditTime: 2023-12-24 02:11:25
+ * @LastEditTime: 2023-12-25 02:05:17
  * @FilePath: /threejs-demo/src/particle/galaxy.js
  */
 import {
@@ -17,11 +17,11 @@ import {
     Vector3,
 } from '../lib/three/three.module.js';
 import {
-    initRenderer, 
-    resize, 
-    initScene, 
-    initGUI, 
-    initOrbitControls, 
+    initRenderer,
+    resize,
+    initScene,
+    initGUI,
+    initOrbitControls,
     initPerspectiveCamera,
     initCoordinates
 } from '../lib/tools/index.js';
@@ -38,7 +38,7 @@ function init() {
 
     const camera = initPerspectiveCamera(new Vector3(10, 10, 10));
     camera.lookAt(0, 0, 0);
-    camera.up.set(0,0,1);
+    camera.up.set(0, 0, 1);
 
     const scene = initScene();
 
@@ -51,11 +51,16 @@ function init() {
     renderer.setAnimationLoop(render);
 
     const params = {
-        count: 300,
+        count: 10000,
         branch: 3,
         radius: 5,
+        scale: 0.5
     }
-    const material = new PointsMaterial({ color: 0xff0000, size: 0.1 });
+    const material = new PointsMaterial({
+        // color: 0xff0000,
+        size: 0.1,
+        vertexColors: true
+    });
 
     const points = new Points(new BufferGeometry(), material);
 
@@ -75,40 +80,89 @@ function init() {
         renderer.render(scene, camera);
     }
 
+    const gui = initGUI();
+    gui.add(params, 'count', 1000, 50000, 10).onChange(updateGeometry);
+    gui.add(params, 'branch', 1, 10, 1).onChange(updateGeometry);
+    gui.add(params, 'radius', 5, 10, 0.1).onChange(updateGeometry);
+    gui.add(params, 'scale', 0, 3, 0.1).onChange(updateGeometry);
+
 }
 
 function createGeometry(params) {
 
-    const {sin,cos,random} = Math;
+    const { sin, cos, random, pow, floor } = Math;
 
     const geometry = new BufferGeometry();
 
-    const { count, radius, branch } = params
+    const { count, radius, branch, scale } = params
     const position = new Float32Array(count * 3);
 
-    const hr = radius * 0.5;
     const angleOfBranch = (Math.PI * 2) / branch;
 
+    // 0,0.1,0.3,0.8,1
+
+    const color = new Float32Array(count * 3);
+
     for (let i = 0; i < count; i++) {
+        const r = random() * radius * pow(random(), 3);
 
         const angle = (i % branch) * angleOfBranch;
 
-        const r = random() * radius;
+        const offset = scale * r;
 
-        const x = r * cos(angle);
-        const y = r * sin(angle);
-        const z = 0;
+        const total = angle + offset;
+
+        const offfetHeight = (radius - r);
+
+        const randX = (pow(random() * 2 - 1, 3) * offfetHeight) / radius;
+        const randY = (pow(random() * 2 - 1, 3) * offfetHeight) / radius;
+        const randZ = (pow(random() * 2 - 1, 3) * offfetHeight) / radius;
+
+        const x = r * cos(total) + randX;
+        const y = r * sin(total) + randY;
+        const z = randZ;
 
         position[i * 3] = x;
         position[i * 3 + 1] = y;
         position[i * 3 + 2] = z;
+
+        const scaleR = r / radius;
+        const index = (scaleR / 5)//floor(scaleR / 50);
+        const lerp = (scaleR) % 0.2;
+
+        console.log(index, lerp);
+
+
+        const vertexColor = getColor(index, lerp);
+
+        color[i * 3] = vertexColor.r;
+        color[i * 3 + 1] = vertexColor.g;
+        color[i * 3 + 2] = vertexColor.b;
     }
 
-    // const color = new Float32Array(count * 3);
 
     geometry.setAttribute('position', new BufferAttribute(position, 3));
+    geometry.setAttribute('color', new BufferAttribute(color, 3));
 
     return geometry;
 
 }
 
+
+function getColor(index, lerp) {
+    const colorHash = ['#ffffff', '#f5f6fa', '#e7d9cd', '#90abbc', '#212c31', '#32313e'];
+    const color = new Color();
+    const lerpColor = new Color();
+
+    return ((index, lerp) => {
+        color.set(colorHash[index]);
+        lerpColor.set(colorHash[index + 1]);
+        color.lerp(lerpColor, lerp);
+        return {
+            r: color.r,
+            g: color.g,
+            b: color.b
+        }
+    })(index, lerp)
+
+}
