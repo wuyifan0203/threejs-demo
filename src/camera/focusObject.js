33e0217a295df767a8db20ee58c5f4d270f4fc95
wuyifan0203 <1208097313@qq.com>
2024-01-17 21:02:25 +0800

    feat:给focusObject添加动画

--- src/camera/focusObject.js
+++ src/camera/focusObject.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2023-12-05 13:43:51
  * @LastEditors: Yifan Wu 1208097313@qq.com
- * @LastEditTime: 2023-12-27 13:15:40
+ * @LastEditTime: 2024-01-17 20:30:46
  * @FilePath: /threejs-demo/src/camera/focusObject.js
  */
 
@@ -14,7 +14,6 @@ import {
     Vector3,
     Matrix4,
     Box2,
-    Quaternion,
 } from '../lib/three/three.module.js';
 import {
     initCoordinates,
@@ -28,12 +27,16 @@ import {
     initGUI
 } from '../lib/tools/index.js';
 import { createNDCMatrix } from '../lib/tools/math.js';
+import { Tween, Easing, update } from '../lib/other/tween.esm.js'
 
 window.onload = () => {
     init();
 };
 
 const ndcMatrix = new Matrix4();
+const box = new Box3();
+const tempBox3 = new Box3();
+const objectsBox2 = new Box2();
 
 function init() {
     const renderer = initRenderer();
@@ -53,7 +56,7 @@ function init() {
     resize(renderer, camera);
 
     initCustomGrid(scene);
-    
+
     const coord = initCoordinates();
 
     orbitControls.addEventListener('change', () => {
@@ -61,7 +64,6 @@ function init() {
     })
 
     function render() {
-        console.log('times');
         renderer.clear();
 
         light.position.copy(camera.position);
@@ -69,6 +71,8 @@ function init() {
         renderer.render(scene, camera);
         renderer.render(coord, camera)
     }
+
+
 
     const redMaterial = new MeshLambertMaterial({ color: 'red' });
     const greenMaterial = new MeshLambertMaterial({ color: 'green' });
@@ -102,7 +106,8 @@ function init() {
         focus() {
             focusObject(this.target === -1 ? meshes : [meshes[this.target]]);
             render();
-        }
+        },
+        useTween: false
     }
     const gui = initGUI();
     gui.add(o, 'target', {
@@ -113,7 +118,14 @@ function init() {
         Yellow: 3
     });
 
+    const updateTween = () => {
+        update();
+    };
+
     gui.add(o, 'focus').name('Focus Objects');
+    gui.add(o, 'useTween').name('Use Tween').onChange((e) => {
+        renderer.setAnimationLoop(e ? updateTween : null)
+    });
 
     createNDCMatrix(window.innerWidth, window.innerHeight, ndcMatrix);
 
@@ -121,111 +133,133 @@ function init() {
         createNDCMatrix(window.innerWidth, window.innerHeight, ndcMatrix)
     });
 
+
+    let tween = null;
+
+    function focusObject(objects) {
+        box.makeEmpty();
+        objectsBox2.makeEmpty();
+
+        objects.forEach(mesh => {
+            tempBox3.makeEmpty();
+            if (!mesh.geometry.boundingBox) {
+                mesh.geometry.computeBoundingBox();
+            };
+            tempBox3.copy(mesh.geometry.boundingBox);
+            tempBox3.applyMatrix4(mesh.matrixWorld);
+            box.union(tempBox3);
+        });
+
+        const center = new Vector3();
+        const tempZoom = window.camera.zoom;
+
+        const { max, min } = box;
+
+        box.getCenter(center);
+
+        ///
+        ///                         z
+        ///     3 ------ 2          |   y
+        ///    /        /|          | /
+        ///   0 ------ 1 |          |/
+        ///   | 7      | 6          o ---- X
+        ///   |        |/
+        ///   4 ------ 5
+        ///
+        ///
+        const points = [
+            new Vector3(min.x, min.y, max.z),
+            new Vector3(max.x, min.y, max.z),
+            new Vector3(max.x, max.y, max.z),
+            new Vector3(min.x, max.y, max.z),
+            new Vector3(min.x, min.y, min.z),
+            new Vector3(max.x, min.y, min.z),
+            new Vector3(max.x, max.y, min.z),
+            new Vector3(min.x, max.y, min.z),
+        ];
+
+        points.forEach((v) => {
+            v.applyMatrix4(window.camera.matrixWorldInverse);
+            v.applyMatrix4(window.camera.projectionMatrix);
+            v.applyMatrix4(ndcMatrix);
+
+            objectsBox2.max.x = Math.max(objectsBox2.max.x, v.x);
+            objectsBox2.min.x = Math.min(objectsBox2.min.x, v.x);
+
+            objectsBox2.max.y = Math.max(objectsBox2.max.y, v.y);
+            objectsBox2.min.y = Math.min(objectsBox2.min.y, v.y);
+        })
+
+
+        const { innerWidth: width, innerHeight: height } = window;
+
+        const [boxWidth, boxHeight] = [
+            objectsBox2.max.x - objectsBox2.min.x,
+            objectsBox2.max.y - objectsBox2.min.y
+        ];
+
+        const [targetWidth, targetHeight] = [
+            width * 0.8,
+            height * 0.8
+        ]
+
+        const zoom = Math.min(
+            (targetWidth * tempZoom) / boxWidth,
+            (targetHeight * tempZoom) / boxHeight,
+        );
+
+        const radius = camera.position.distanceTo(orbitControls.target);
+
+
+        const direction = new Vector3().subVectors(camera.position, orbitControls.target).normalize();
+
+        const position = direction.multiplyScalar(radius).add(center);
+
+
+        // tween.
+        if (o.useTween) {
+            tween = new Tween({
+                px: camera.position.x,
+                py: camera.position.y,
+                pz: camera.position.z,
+                cx: orbitControls.target.x,
+                cy: orbitControls.target.y,
+                cz: orbitControls.target.z,
+                zoom: camera.zoom
+            })
+
+            tween.to({
+                px: position.x,
+                py: position.y,
+                pz: position.z,
+                cx: center.x,
+                cy: center.y,
+                cz: center.z,
+                zoom: zoom,
+            }, 1000).easing(Easing.Quartic.Out);
+
+            tween.onUpdate((e) => {
+                orbitControls.object.position.set(e.px, e.py, e.pz);
+                orbitControls.target.set(e.cx, e.cy, e.cz);
+                orbitControls.object.zoom = e.zoom;
+                orbitControls.update();
+            })
+
+            tween.start();
+        } else {
+            orbitControls.object.position.copy(position);
+            orbitControls.object.zoom = zoom;
+            orbitControls.target.copy(center);
+            orbitControls.update();
+        }
+
+
+
+    }
+
 }
-const box = new Box3();
-const tempBox3 = new Box3();
-const objectsBox2 = new Box2();
-
-function focusObject(objects) {
-    box.makeEmpty();
-    objectsBox2.makeEmpty();
-
-    objects.forEach(mesh => {
-        tempBox3.makeEmpty();
-        if (!mesh.geometry.boundingBox) {
-            mesh.geometry.computeBoundingBox();
-        };
-        tempBox3.copy(mesh.geometry.boundingBox);
-        tempBox3.applyMatrix4(mesh.matrixWorld);
-        box.union(tempBox3);
-    });
-
-    const center = new Vector3();
-    const tempZoom = window.camera.zoom;
-
-    const { max, min } = box;
-
-    box.getCenter(center);
-
-    ///
-    ///                         z
-    ///     3 ------ 2          |   y
-    ///    /        /|          | /
-    ///   0 ------ 1 |          |/
-    ///   | 7      | 6          o ---- X
-    ///   |        |/
-    ///   4 ------ 5
-    ///
-    ///
-    const points = [
-        new Vector3(min.x, min.y, max.z),
-        new Vector3(max.x, min.y, max.z),
-        new Vector3(max.x, max.y, max.z),
-        new Vector3(min.x, max.y, max.z),
-        new Vector3(min.x, min.y, min.z),
-        new Vector3(max.x, min.y, min.z),
-        new Vector3(max.x, max.y, min.z),
-        new Vector3(min.x, max.y, min.z),
-    ];
-
-    points.forEach((v) => {
-        v.applyMatrix4(window.camera.matrixWorldInverse);
-        v.applyMatrix4(window.camera.projectionMatrix);
-        v.applyMatrix4(ndcMatrix);
-
-        objectsBox2.max.x = Math.max(objectsBox2.max.x, v.x);
-        objectsBox2.min.x = Math.min(objectsBox2.min.x, v.x);
-
-        objectsBox2.max.y = Math.max(objectsBox2.max.y, v.y);
-        objectsBox2.min.y = Math.min(objectsBox2.min.y, v.y);
-    })
 
 
-    const { innerWidth: width, innerHeight: height } = window;
 
-    const [boxWidth, boxHeight] = [
-        objectsBox2.max.x - objectsBox2.min.x,
-        objectsBox2.max.y - objectsBox2.min.y
-    ];
-
-    const [targetWidth, targetHeight] = [
-        width * 0.8,
-        height * 0.8
-    ]
-
-    console.log({
-        pos: window.camera.position.clone(),
-        zoom: window.camera.zoom,
-        rotation: window.camera.rotation.clone(),
-        up: window.camera.up.clone(),
-    });
-
-    const zoom = Math.min(
-        (targetWidth * tempZoom) / boxWidth,
-        (targetHeight * tempZoom) / boxHeight,
-    );
-
-    const radius = window.camera.position.distanceTo(window.controls.target);
-
-
-    const direction = new Vector3().subVectors(window.camera.position, window.controls.target).normalize();
-
-    const position = direction.multiplyScalar(radius).add(center);
-
-    window.controls.object.position.copy(position);
-    window.controls.object.zoom = zoom;
-    window.controls.target.copy(center);
-
-
-    window.controls.update();
-
-    console.log({
-        pos: window.camera.position.clone(),
-        zoom: window.camera.zoom,
-        rotation: window.camera.rotation.clone(),
-        up: window.camera.up.clone(),
-    });
-
-}
 
 
