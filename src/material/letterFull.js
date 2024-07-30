d8ccd86e10f849fa708cf4c30233ec25be8bd37b
wuyifan0203 <1208097313@qq.com>
2024-07-30 18:10:03 +0800

    feat: add letter full

--- src/material/letterFull.js
+++ src/material/letterFull.js
@@ -10,7 +10,10 @@ import {
     InstancedMesh,
     MeshBasicMaterial,
     MathUtils,
-    InstancedBufferAttribute
+    InstancedBufferAttribute,
+    ShaderMaterial,
+    ShaderLib,
+    UniformsUtils
 } from "../lib/three/three.module.js";
 import { update, Tween } from "../lib/other/tween.esm.js";
 import { SimplexNoise } from "../lib/three/SimplexNoise.js";
@@ -40,6 +43,12 @@ function init() {
 
     const clock = new Clock();
     let t = 0;
+    const maxAnisotropy = renderer.capabilities.getMaxAnisotropy();
+    console.log('maxAnisotropy', maxAnisotropy);
+
+    const terrain = new TextTerrain(maxAnisotropy);
+
+    scene.add(terrain);
 
 
     function render() {
@@ -57,8 +66,7 @@ function init() {
     // const textTextureCanvas = createTextureCanvas(content);
     // document.body.appendChild(textTextureCanvas);
 
-    const maxAnisotropy = renderer.capabilities.getMaxAnisotropy();
-    console.log('maxAnisotropy',maxAnisotropy);
+
 }
 
 
@@ -90,229 +98,64 @@ class TextTerrain extends Object3D {
         const textTextureCanvas = createTextureCanvas(content);
         const canvasTexture = new CanvasTexture(textTextureCanvas);
         canvasTexture.anisotropy = anisotropy;
-    
-    }
-}
 
-
-
-class TextTerrain2 extends Object3D {
-    constructor(anisotropy) {
-        super();
-        // atlas
-        let alphabet = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", '爱'];
-        let textTexture = (() => {
-            let c = document.createElement("canvas");
-            let ctx = c.getContext("2d");
-            let texSize = 2048;
-            c.width = texSize;
-            c.height = texSize;
-            ctx.clearRect(0, 0, texSize, texSize);
-            let dim = 8;
-            let dimStep = texSize / dim;
-            for (let i = 0; i < alphabet.length; i++) {
-                let tileX = i % 8;
-                let tileY = Math.floor(i / 8);
-                let x = (tileX + 0.5) * dimStep;
-                let y = texSize - (tileY + 0.5) * dimStep;
-                ctx.fillStyle = `rgba(0, 0, 0, 1)`;
-                ctx.textAlign = "center";
-                ctx.textBaseline = "middle";
-                ctx.font = `bold ${dimStep * 0.9}px Arial`;
-                ctx.fillText(alphabet[i], x, y);
-            }
-            let tex = new CanvasTexture(c);
-            tex.colorSpace = "srgb";
-            tex.anisotropy = anisotropy;
-            return tex;
-        })(); // atlas
-
-        let tileDim = 200;
-
-        let g = new PlaneGeometry();
-        g.setAttribute(
-            "letterIdx",
-            new InstancedBufferAttribute(
-                new Float32Array(
-                    Array.from(
-                        {
-                            length: tileDim * tileDim
-                        },
-                        () => {
-                            return MathUtils.randInt(0, alphabet.length - 1);
-                        }
-                    )
-                ),
-                1
-            )
-        );
-
-        let m = new MeshBasicMaterial({
-            map: textTexture,
-            transparent: true,
-            alphaTest: 0.01,
-            side: 2,
-            //forceSinglePass: true,
-            onBeforeCompile: (shader) => {
-                shader.vertexShader = `
-        	attribute float letterIdx;
-          varying float vLetterIdx;
-        	${shader.vertexShader}
-        `.replace(
-                    `#include <uv_vertex>`,
-                    `#include <uv_vertex>
-          vLetterIdx = letterIdx;
-          `
-                );
-                //console.log(shader.vertexShader);
-
-                shader.fragmentShader = `
-        	varying float vLetterIdx;
-          ${shader.fragmentShader}
-        `.replace(
-                    `#include <map_fragment>`,
-                    `
-          float letterIdx = floor(vLetterIdx + 0.1);
-          float tileStep = 1. / 8.;
-          float u = mod(letterIdx, 8.);
-          float v = floor(letterIdx / 8.);
-          vec2 iUv = (vec2(u, v) + vMapUv) * tileStep;
-          vec4 sampledDiffuseColor = texture2D( map, iUv );
-					diffuseColor *= sampledDiffuseColor;
-          `
-                );
-                //console.log(shader.fragmentShader);
-            }
-        });
-        let io = new InstancedMesh(g, m, tileDim * tileDim);
-        this.instancedMesh = io;
-
-        this.dummy = new Object3D();
-
-        this.finals = [];
-
-        let tri = new Triangle();
-        let n = new Vector3();
-        let la = new Vector3();
-
-        function getY(x, z) {
-            return simplex.noise(x * 0.01, z * 0.01) * 7.5;
+        const tileNum = 200;
+        const letterIdxArray = new Float32Array(tileNum * tileNum);
+        for (let i = 0, l = letterIdxArray.length, max = content.length - 1; i < l; i++) {
+            letterIdxArray[i] = MathUtils.randInt(0, max);
         }
 
-        let setFinals = () => {
-            let y0 = getY(this.dummy.position.x, this.dummy.position.z);
-            let y1 = getY(this.dummy.position.x, this.dummy.position.z - 1);
-            let y2 = getY(this.dummy.position.x + 1, this.dummy.position.z);
-            this.dummy.position.y = y0;
+        const geometry = new PlaneGeometry();
+        geometry.setAttribute('letterId', new InstancedBufferAttribute(letterIdxArray, 1));
 
-            tri.a.set(this.dummy.position.x, y1, this.dummy.position.z - 1);
-            tri.b.set(this.dummy.position.x, y0, this.dummy.position.z);
-            tri.c.set(this.dummy.position.x + 1, y2, this.dummy.position.z);
-            tri.getNormal(n);
+        console.log(geometry);
 
-            la.copy(this.dummy.position).add(n);
-            this.dummy.lookAt(la);
-            this.dummy.rotation.z = 0; // align along Z-axis of the terrain
-            this.dummy.updateMatrix();
 
-            this.finals.push({
-                y: y0,
-                pos: this.dummy.position.clone(),
-                rot: this.dummy.rotation.clone(),
-                dummy: new Object3D(),
-                inAction: false,
-                mediators: {
-                    v: new Vector3(),
-                    v2: new Vector3()
-                }
-            });
-        };
+        const material = new MeshBasicMaterial({
+            side: 2,
+            map: canvasTexture,
+            transparent: true,
+            alphaTest: 0.01,
+        });
 
-        // make it grid
-        for (let z = 0; z < tileDim; z++) {
-            for (let x = 0; x < tileDim; x++) {
-                this.dummy.position.x = -(tileDim - 1) * 0.5 + x;
-                this.dummy.position.z = -(tileDim - 1) * 0.5 + z;
-                setFinals(this.dummy.position);
-                this.instancedMesh.setMatrixAt(z * tileDim + x, this.dummy.matrix);
-            }
-        } // make it grid
+        material.onBeforeCompile = (shader) => {
 
-        this.add(io);
+            console.log(shader);
+            shader.vertexShader = /*glsl*/`
+                    attribute float letterIdx;
+                    varying float vLetterIdx;
+                    ${shader.vertexShader}
+                `.replace(
+                   /*glsl*/`#include <map_fragment>`,
+                   /*glsl*/`
+                        #include <map_fragment>;
+                        vLetterIdx = letterIdx;
+                   `
+            );
+            shader.fragmentShader = /*glsl*/`
+                    varying float vLetterIdx;
+                    ${shader.fragmentShader}
+                `.replace(
+                    /*glsl*/`#include <map_fragment>`,
+                    /*glsl*/`
+                        float letterIdx = floor(vLetterIdx + 0.1);
+                        float tileStep = 1.0 / 8.0;
+                        float u = mod(letterIdx , 8.0);
+                        float v = floor(letterIdx / 8.0);
+                        vec2 tileUV = (vec2(u,v) + vMapUv) * tileStep;
+                        vec4 sampledDiffuseColor = texture2D( map, tileUV );
+            			diffuseColor *= sampledDiffuseColor;
+                    `
+            )
+        }
 
-        // actions section
-        this.actions = Array.from(
-            {
-                length: 5000
-            },
-            () => {
-                let action = (delay) => {
-                    //console.log("action started");
-                    let getFreeLetterIndex = () => {
-                        let letterIndex = Math.floor(Math.random() * this.finals.length);
-                        if (!this.finals[letterIndex].inAction) {
-                            return letterIndex;
-                        } else {
-                            return getFreeLetterIndex();
-                        }
-                    };
+        console.log(material);
 
-                    let freeLetterIndex = getFreeLetterIndex();
-                    let freeLetter = this.finals[freeLetterIndex];
-                    let height = 30;
-                    let m = freeLetter.mediators;
-                    let v = m.v;
-                    let v2 = m.v2;
+        const instanceMesh = new InstancedMesh(geometry, material, tileNum * tileNum);
 
-                    v2.random()
-                        .multiplyScalar(0.5)
-                        .addScalar(0.5)
-                        .multiplyScalar(Math.PI * 3 * Math.sign(Math.random() - 0.5));
-
-                    let tween = new Tween({
-                        val: 0
-                    })
-                        .to(
-                            {
-                                val: 1
-                            },
-                            10000
-                        )
-                        .delay(delay)
-                        .onStart(() => {
-                            freeLetter.inAction = true;
-                        })
-                        .onUpdate((val) => {
-                            v.lerpVectors(v2, freeLetter.rot, val.val);
-                            freeLetter.dummy.rotation.set(v.x, v.y, v.z);
-                            freeLetter.dummy.position.copy(freeLetter.pos);
-                            freeLetter.dummy.position.y = THREE.MathUtils.lerp(
-                                height,
-                                freeLetter.y,
-                                val.val
-                            );
-                            freeLetter.dummy.updateMatrix();
-                            io.setMatrixAt(freeLetterIndex, freeLetter.dummy.matrix);
-                        })
-                        .onComplete(() => {
-                            freeLetter.inAction = false;
-                            action(Math.random() * 10000);
-                        });
-                    tween.start();
-                };
-                return action;
-            }
-        ); // actions section
-
-        //console.log(this.actions)
+        this.add(instanceMesh);
     }
 }
 
 
-
-// let textTerrain = new TextTerrain(renderer.capabilities.getMaxAnisotropy());
-// scene.add(textTerrain);
-// textTerrain.actions.forEach((action) => {
-//     action((Math.random() * 0.9 + 0.1) * 10000);
-// }); // start the actions
 
