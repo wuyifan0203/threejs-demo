4da83871989fbbc0d564780ee8be029929c7c610
wuyifan0203 <1208097313@qq.com>
2024-09-05 10:14:30 +0800

    ci: :ferris_wheel: 添加preview 为静态部署做准备

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,28 @@
 /*
  * @Date: 2024-02-01 16:01:31
- * @LastEditors: Yifan Wu 1208097313@qq.com
- * @LastEditTime: 2024-02-01 16:09:15
+ * @LastEditors: wuyifan0203 1208097313@qq.com
+ * @LastEditTime: 2024-09-05 10:10:50
  * @FilePath: /threejs-demo/vite.config.ts
  */
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 export default defineConfig({
     root:'./',
     base:'/threejs-demo/',
+    plugins:[
+        viteStaticCopy({
+            targets:[
+                {
+                    src:'screenshots',
+                    dest:'./'
+                }
+            ]
+        })
+    ],
     server:{
         port:6600
     },
     build:{
-        outDir:'docs',
+        outDir:'dist',
     }
 })
