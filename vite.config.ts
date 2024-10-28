346244be9cfa1df5c839eb3feabcc804812816b0
wuyifan0203 <1208097313@qq.com>
2024-10-28 10:28:28 +0800

    docs: :pencil: update docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 /*
  * @Date: 2024-02-01 16:01:31
  * @LastEditors: wuyifan0203 1208097313@qq.com
- * @LastEditTime: 2024-09-05 10:10:50
- * @FilePath: /threejs-demo/vite.config.ts
+ * @LastEditTime: 2024-10-28 10:08:35
+ * @FilePath: \threejs-demo\vite.config.ts
  */
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy'
@@ -20,7 +20,7 @@ export default defineConfig({
         })
     ],
     server:{
-        port:6600
+        port:6500
     },
     build:{
         outDir:'dist',
